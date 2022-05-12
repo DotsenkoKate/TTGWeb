c8f8f59895429071be0f686f84c0dd29100bd869
Екатерина Доценко <dotsenko.kate@yandex.ru>
2022-05-12 22:29:54 +0300

    Map Fix

--- TTGWeb/wwwroot/js/site.js
+++ TTGWeb/wwwroot/js/site.js
@@ -1,9 +1,11 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-const { deserialize } = require("v8");
+//const { deserialize } = require("v8");
 
 // Write your JavaScript code.
+
+var myMap;
 
 function show(param_div_id) {
     document.getElementById('main').innerHTML = document.getElementById(param_div_id).innerHTML;
@@ -102,34 +104,64 @@ function init() {
     // Создание карты.
     var myMap = new ymaps.Map("map98", {
         center: [48.717987, 44.481111],
-        zoom: 12
+        zoom: 12,
+        controls: ['fullscreenControl', 'zoomControl']
     });
+    addItems(myMap);
+
+
+setInterval(autoupdate, 15000);
+    
+function autoupdate()
+    {
+        
+        myMap.destroy();
+        myMap = null;
+        init();
+    }
+
+   
+}
+function addItems(myMap) {
     myMap.geoObjects
         .add(new ymaps.Placemark([48.724803, 44.468239], {
             balloonContent: 'ул. Добрая'
         }, {
-            preset: 'islands#icon',
+            preset: "islands#circleIcon",
             iconColor: '#0095b6'
         }))
         .add(new ymaps.Placemark([48.721234, 44.473542], {
             balloonContent: 'ул. Красивая'
         }, {
-            preset: 'islands#icon',
+            preset: "islands#circleIcon",
             iconColor: '#0095b6'
         }))
         .add(new ymaps.Placemark([48.717174, 44.483618], {
             balloonContent: 'ул. Умная'
         }, {
-            preset: 'islands#icon',
+            preset: "islands#circleIcon",
             iconColor: '#0095b6'
         }))
         .add(new ymaps.Placemark([48.713997, 44.496888], {
             balloonContent: 'ул. Ленина'
         }, {
-            preset: 'islands#icon',
+            preset: "islands#circleIcon",
             iconColor: '#0095b6'
         }))
+        .add(new ymaps.Placemark([48.714849, 44.493857], {
+            balloonContent: 'Петров Петр Петрович'
+        }, {
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: '/images/busPoint.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15]
+        }))
 
+/*
     var myPolyline = new ymaps.Polyline([
         // Указываем координаты вершин.
         [48.724803, 44.468239],
@@ -144,65 +176,27 @@ function init() {
     });
 
     // Добавляем линию на карту.
-    myMap.geoObjects.add(myPolyline);
+    myMap.geoObjects.add(myPolyline);*/
+
+    var multiRoute = new ymaps.multiRouter.MultiRoute({
+        referencePoints: [[48.724803, 44.468239],
+        [48.721234, 44.473542],
+        [48.717174, 44.483618],
+        [48.713997, 44.496888]]
+    }, {
 
 
-    var myMap = new ymaps.Map("map10c", {
-        center: [48.717987, 44.481111],
-        zoom: 12
+        wayPointVisible: false,
+        routeStrokeWidth: 5,
+        boundsAutoApply: true,
+        zoomMargin: 30
     });
-    myMap.geoObjects
-        .add(new ymaps.Placemark([48.714345, 44.498680], {
-            balloonContent: 'ул. Злая'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#0095b6'
-        }))
-        .add(new ymaps.Placemark([48.719656, 44.507404], {
-            balloonContent: 'ул. Страшная'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#0095b6'
-        }))
-        .add(new ymaps.Placemark([48.724273, 44.514624], {
-            balloonContent: 'ул. Тупая'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#0095b6'
-        }))
-        .add(new ymaps.Placemark([48.719940, 44.519983], {
-            balloonContent: 'ул. Маркса'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#0095b6'
-        }))
-
-    var myPolyline = new ymaps.Polyline([
-        // Указываем координаты вершин.
-        [48.714345, 44.498680],
-        [48.719656, 44.507404],
-        [48.724273, 44.514624],
-        [48.719940, 44.519983]
-    ], {}, {
-        // Ширину линии.
-        strokeWidth: 4,
-        // Добавляем в контекстное меню новый пункт, позволяющий удалить ломаную.
-
-    });
-
-    // Добавляем линию на карту.
-    myMap.geoObjects.add(myPolyline);
-
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-    })
+    myMap.geoObjects.add(multiRoute);
 }
-
 
 function init1() {
     
-    
-    var myMap = new ymaps.Map("map", {
+   var  myMap = new ymaps.Map("map", {
         center: [48.717987, 44.481111],
         zoom: 11
     }, {
@@ -239,41 +233,21 @@ function init1() {
             preset: 'islands#icon',
             iconColor: '#0095b6'
         }))
-    var myPolyline = new ymaps.Polyline([
-        // Указываем координаты вершин.
-        [48.724803, 44.468239],
+    var multiRoute = new ymaps.multiRouter.MultiRoute({
+        referencePoints: [[48.724803, 44.468239],
         [48.721234, 44.473542],
         [48.717174, 44.483618],
-        [48.713997, 44.496888]
-    ], {}, {
-        // Ширину линии.
-        strokeWidth: 3,
+        [48.713997, 44.496888]]
+    }, {
 
+
+        wayPointVisible: false,
+        routeStrokeWidth: 5,
+        boundsAutoApply: true,
+        zoomMargin: 30
     });
 
-    // Добавляем линию на карту.
-    myMap.geoObjects.add(myPolyline);
-    // Обработка события, возникающего при щелчке
-    // левой кнопкой мыши в любой точке карты.
-    // При возникновении такого события откроем балун.
-    /*	myMap.events.add('click', function (e) {
-            if (!myMap.balloon.isOpen()) {
-                var coords = e.get('coords');
-                myMap.balloon.open(coords, {
-                    contentHeader: 'Место!',
-                    contentBody: '<p>Координаты точки: ' + [
-                            coords[0].toPrecision(6),
-                            coords[1].toPrecision(6)
-                    ].join(', ') + '</p>'
-                });
-                [coords[0].toPrecision(6),
-                    coords[1].toPrecision(6)
-                ].join(', ');
-            }
-            else {
-                myMap.balloon.close();
-            }
-        });
+
         /* Начальный адрес метки */
 
     var address = 'Россия, Волгоград, Ленина, д. 28';
@@ -301,15 +275,28 @@ function init1() {
         });
 
         myMap.geoObjects.add(myPlacemark);
+        myMap.geoObjects.add(multiRoute);
         myMap.setCenter(coord, 12);
     });
+    document.getElementById('redrawMap').onclick = function () {
+        // Для уничтожения используется метод destroy.
+        myMap.destroy();// Деструктор карты
+        myMap = null;
+        ShowMapEditigRoute();
+    }
+}
+function ShowMapEditigRoute() {
 
+    ymaps.ready(init2);
+
+    function init2() {
+
+        init1();
+
+    }
+    
 }
 
-function test()
-{
-    ymaps.ready(init1);
 
-}
-
+    
 
